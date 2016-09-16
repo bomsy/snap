14e4cfcf47847db22e63b155986e55ff1e672209
bomsy <b4bomsy@gmail.com>
2016-09-16 12:06:37 +0100

    add example app, add postinstall script, write run action

---
+++ scripts/postinstall.js
@@ -0,0 +1,35 @@
+var exec = require('child-process-promise').exec;
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs-extra'));
+
+var cwd = process.cwd();
+
+var swfile = 'npm-shrinkwrap.json';
+var dir = '.snapshots/';
+var config = dir + 'config.json'
+
+function timestampFile(filename, ts) {
+  var fname = filename.split('.');
+  fname[0] = fname[0] + '-' + ts;
+  return fname.join('.');
+}
+var timestamp = new Date().getTime();
+exec('npm shrinkwrap')
+  .then(fs.ensureDirAsync(dir))
+  .then(function() {
+    return fs.moveAsync(swfile, dir + timestampFile(swfile, timestamp));
+  })
+  .then(function() {
+    return fs.readJsonAsync(config)
+  })
+  .then(function(configJson) {
+    configJson.previous = configJson.latest;
+    configJson.latest = timestamp;
+    return fs.writeJsonAsync(config, configJson);
+  })
+  .then(function() {
+    console.log('done');
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
